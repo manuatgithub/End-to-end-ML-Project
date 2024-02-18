b5dc8b570f9b39b07bbac0b75d0f11f636a3b997
amusprojects <kripz.rm@gmail.com>
2024-02-18 07:02:55 +0530

    Data ingestion Complete

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,15 +1,13 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlProject import logger
+from src.mlProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
-
-
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
